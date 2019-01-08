c1931a10c67d006ec7883b080e18c5b93eda124f
debbabi <dbassem@gmail.com>
2019-01-08 13:48:14 +0100

    Update tests-libs.js

--- js/tests-libs.js
+++ js/tests-libs.js
@@ -1,3 +1,6 @@
-var testSuccessTrue() = function() {
-  pm.test('Success is true', true);
-}
+var testPayloadOk = function() {
+  pm.test("Payload is OK", function() {
+    pm.response.to.have.jsonBody("success", true);
+    pm.response.to.have.jsonBody("data");
+  });
+};
