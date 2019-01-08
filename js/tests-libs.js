c516a4b14ff26fe51d181b26df0abca65cdfb40b
debbabi <dbassem@gmail.com>
2019-01-08 14:12:10 +0100

    Update tests-libs.js

--- js/tests-libs.js
+++ js/tests-libs.js
@@ -1,3 +1,6 @@
+/*
+ * Check if returned Payload is a valid JSON, has 'success' field set to 'true', and has 'data' field.
+ */
 var testPayloadOk = function() {
   pm.test("Payload is OK", function() {
     pm.response.to.have.jsonBody("success", true);
