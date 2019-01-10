a828f071a74c5f2ca1b8f28743f81adc65845dbb
vviannay <vincent.viannay@agilium.com>
2019-01-10 12:14:13 +0100

    add description

--- js/agllibs.js
+++ js/agllibs.js
@@ -1,11 +1,12 @@
-/** Response should be a JSON
+/** 
+ * @testResponseOk
+ * Response should be a JSON
  * Response must have jsonBody.Success
  * Response must have a jsonBody and properties data
  * @param none
  */
 
 var testResponseOk= function() {
-
 	pm.test("Response is OK", function() {
 		pm.response.to.be.success
 		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
@@ -16,11 +17,11 @@ var testResponseOk= function() {
 	pm.test("Payload is OK", function() {
 		pm.response.to.have.jsonBody("success", true)
 		pm.response.to.have.jsonBody("data")
-	});
-	
+	});	
 }  
 
 /**
+ * @testDataOk
  Response Must have jsonBody.data.id
  * @param id
  */
@@ -39,6 +40,12 @@ var testDataOk = function(id) {
 
 
  /**
+  * @testLoginOk
+  * Response should be Success
+  * Response must have header "content-type", "application/json"
+  * Response must have a jsonBody
+  * Response must have a jsonBody.data
+  * Cookie SSO must be generate
  * @param none
  */
 
@@ -60,8 +67,16 @@ var testDataOk = function(id) {
 	
 }
 	
+/** 
+ * @testLogoutOk
+ * Response should be success 
+ * Response must have header "content-type", "application/json"
+ * Response must have a jsonBody
+ * Response must have jsonBody.data
+ * Cookie SSO must be removed
+ * @param
+ */
 var  testLogoutOk = function() {
-
 	pm.test("Response is OK", ()=>{
 		pm.response.to.be.success
 		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
@@ -77,5 +92,5 @@ var  testLogoutOk = function() {
 		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.false
 	});
 	
-	}
+}
 	
