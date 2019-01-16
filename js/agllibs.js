b8f997de26f0890c1e55b3fb54884bc93761edc1
vviannay <vincent.viannay@agilium.com>
2019-01-16 12:22:57 +0100

    agllibs

--- js/agllibs.js
+++ js/agllibs.js
@@ -3,8 +3,8 @@ var TestLoginOk = function(enterprise, roleDimensions) {
 		pm.response.to.be.success
 		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
 		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-		pm.response.to.have.jsonBody()
 	})
+
 	pm.test("Data is OK", function(){
 		pm.response.have.jsonBody('data.account.profile.societe.label', enterprise);
 		pm.response.have.jsonBody('data.account.profile.societe.rolesDimensions.id', roleDimensions)
