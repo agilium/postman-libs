de2e7eb63dab4f1878ec6e603e82dd5c05502020
vviannay <vincent.viannay@agilium.com>
2019-01-21 14:12:29 +0100

    maj

--- js/agllibs.js
+++ js/agllibs.js
@@ -1,30 +1,55 @@
-/** 
- * @testResponseOk
- * Response should be a JSON
- * Response must have jsonBody.Success
- * Response must have a jsonBody and properties data
- * @param none
- */
+var TestLoginIsOk = function(user) {
+	let jsonBody = pm.response.json();
+	let roleDimensions;
 
-var testResponseOk= function() {
-	pm.test("Response is OK", function() {
-		pm.response.to.be.success
-		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
+	for (role of jsonBody.data.profile.societe.roleDimensions) {
+		if (role.id == "absged.depositaireAvecValidation") {
+			roleDimensions = role.id;
+			break;
+		}
+	}
+	console.log(roleDimensions);
+	pm.test("Response is Success, and it's a JSON", function(){
+		pm.response.to.be.success;
 		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
 		pm.response.to.have.jsonBody()
 	})
-	
+
+	pm.test("Response generated Cookie AGL_LSSO", function() {
+		
+		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
+		
+	});
+
 	pm.test("Payload is OK", function() {
 		pm.response.to.have.jsonBody("success", true)
 		pm.response.to.have.jsonBody("data")
 	});	
-}  
 
-/**
- * @testDataOk
- * Response Must have jsonBody.data.id
- * @param id
- */
+	pm.test("Data account contain username", function(){
+			pm.response.to.have.jsonBody("data.account.login", user) 
+	})
+
+	pm.test("RoleDimensions is: 'absged.depositaireAvecValidation'.", function(){
+		pm.expect(roleDimensions).to.eql("absged.depositaireAvecValidation");
+	});
+}
+
+
+
+
+var testReferencialIsOk = function(idReferencial) {
+	pm.test('Referencial exist', function(){
+		pm.response.to.have.jsonBody("data[0].id", idReferencial) 
+	})
+}
+
+var testUploadIsOk = function(fileName) {
+	pm.test("Document successfully uploaded", function() {
+		pm.response.to.have.jsonBody("filename", filename) 
+		pm.expect(jsonBody.uploaded).to.be.true
+	})
+}
 
 var testDataOk = function(id) {
 	pm.test('Data is OK', function(){
@@ -33,125 +58,6 @@ var testDataOk = function(id) {
 	});
 }
 
-/** Response must have a jsonBody.data.fkpoiType
- * Response must have a jsonBody.data.fkvalueDef
- * @Param fkpoiType, fkvalueDef
- */
-
-
- /**
-  * @testLoginOk
-  * Response should be Success
-  * Response must have header "content-type", "application/json"
-  * Response must have a jsonBody
-  * Response must have a jsonBody.data
-  * Cookie SSO must be generate
- * @param none
- */
-
- var testloginOk = function() {
-	pm.test("Response is OK", ()=>{
-		pm.response.to.be.success
-		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-		pm.response.to.have.jsonBody()
-	})
-	
-	pm.test("Payload is OK", ()=> {
-		pm.response.to.have.jsonBody("success", true)
-		pm.response.to.have.jsonBody("data")
-	});
-	
-	pm.test("Get Cookie SSO OK", ()=>{
-		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-	});
-	
-}
-	
-/** 
- * @testLogoutOk
- * Response should be success 
- * Response must have header "content-type", "application/json"
- * Response must have a jsonBody
- * Response must have jsonBody.data
- * Cookie SSO must be removed
- * @param
- */
-var  testLogoutOk = function() {
-	pm.test("Response is OK", ()=>{
-		pm.response.to.be.success
-		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-		pm.response.to.have.jsonBody()
-	})
-	
-	pm.test("Payload is OK", ()=> {
-		pm.response.to.have.jsonBody("success", true)
-		pm.response.to.have.jsonBody("data")
-	});
-	
-	pm.test("Cookie SSO Removed OK", ()=>{
-		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.false
-	});
-
-
-	
-}
-
-/** Response should be a JSON
- * Response must have jsonBody.Success
- * Response must have a jsonBody and properties data
- * @param none
- */
-
-var testResponseOk= function() {
-
-	pm.test("Response is OK", function() {
-		pm.response.to.be.success
-		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-		pm.response.to.have.jsonBody()
-	})
-	
-	pm.test("Payload is OK", function() {
-		pm.response.to.have.jsonBody("success", true)
-		pm.response.to.have.jsonBody("data")
-	});
-	
-}  
-
-/**
- Response Must have jsonBody.data.id
- * @param id
- */
-
-var testDataOk = function(id) {
-	pm.test('Data is OK', function(){
-	   pm.response.to.have.jsonBody("data.id", id)
-	   console.log(jsonBody.data.id)
-	});
-}
-
- /**
- * @param none
- */
-
- var testloginOk = function() {
-	pm.test("Response is OK", ()=>{
-		pm.response.to.be.success
-		pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-		pm.response.to.have.jsonBody()
-	})
-	
-	pm.test("Payload is OK", ()=> {
-		pm.response.to.have.jsonBody("success", true)
-		pm.response.to.have.jsonBody("data")
-	});
-	
-	pm.test("Cookie SSO Removed OK", ()=>{
-		pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-	});
-	
-}
-	
 var  testLogoutOk = function() {
 
 	pm.test("Response is OK", ()=>{
@@ -171,67 +77,4 @@ var  testLogoutOk = function() {
 	
 	}
 
-	var TGedLogin = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
-	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
-		
-		pm.test('Data is OK', function(){
-			var jsonBody = pm.response.json();
-				pm.expect(jsonBody.data.account.profile.societe.label).to.equal("PARALU");
-				pm.expect(jsonBody.data.account.profile.societe.rolesDimensions[0].id).to.equal("absged.depositaireAvecValidation");
-		});
-	}
-	
-	var TestRolesDimensions = function(userNameVar, rolesVisit, rolesGed)  {
-		pm.test('data.Auth is OK', function () {
-			pm.response.to.have.jsonBody("data.auth.token", pm.cookies.get("AGL_LSSO")) 
-			pm.response.to.have.jsonBody("data.auth.type", "Bearer");
-		});
-		pm.test('data.account is OK', function () {
-			pm.response.to.have.jsonBody("data.account.login", pm.variables.get(userNameVar)) 
-			pm.response.to.have.jsonBody("data.account.roles") 
-			pm.response.to.have.jsonBody("data.account.profile")
-		});
-		const jsonData = pm.response.json() 
-			pm.test("data.account.role is OK", function () {
-			pm.expect(jsonData.data.account.roles).to.be.an('array').that.includes(pm.globals.get('userRoleVisitProcess'))
-		});
-	
-		
-		pm.test('RolesDimensions are OK', function () {
-			const visitRoles = profile.societe.rolesDimensions.filter(x => x.id.startsWith('absvisit.')).reduce((result, x) => {
-				result.push(x.id);
-				return result
-			}, []) 
-			pm.expect(visitRoles).to.deep.equal(rolesVisit)
-			
-			const gedRoles = profile.societe.rolesDimensions.filter(x => x.id.startsWith('absged.')).reduce((result, x) =>{
-				result.push(x.id);
-				return result
-			}, [])
-			pm.expect(gedRoles).to.deep.equal(rolesGed)
-		});
-	}
-	
-	
-	
-	
-	
-	
 
-
-
-
-
-
-	
