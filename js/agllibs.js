27186cb01626ceb9214c5115f045852d1151bd97
vviannay <vincent.viannay@agilium.com>
2019-01-21 13:58:03 +0100

    maj

--- js/agllibs.js
+++ js/agllibs.js
@@ -192,108 +192,35 @@ var  testLogoutOk = function() {
 		});
 	}
 	
-	var TGetReferencialLots = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
+	var TestRolesDimensions = function(userNameVar, rolesVisit, rolesGed)  {
+		pm.test('data.Auth is OK', function () {
+			pm.response.to.have.jsonBody("data.auth.token", pm.cookies.get("AGL_LSSO")) 
+			pm.response.to.have.jsonBody("data.auth.type", "Bearer");
+		});
+		pm.test('data.account is OK', function () {
+			pm.response.to.have.jsonBody("data.account.login", pm.variables.get(userNameVar)) 
+			pm.response.to.have.jsonBody("data.account.roles") 
+			pm.response.to.have.jsonBody("data.account.profile")
+		});
+		const jsonData = pm.response.json() 
+			pm.test("data.account.role is OK", function () {
+			pm.expect(jsonData.data.account.roles).to.be.an('array').that.includes(pm.globals.get('userRoleVisitProcess'))
+		});
 	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
-
-		pm.test("Lot is OK", function (){
-			jsonBody = pm.response.json();
-			pm.expect(jsonBody.data[0].id).to.equal("FAC");
-		})
-	}
-
-	var TGetReferencialProjectPhase = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
-	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
 		
-		
-		pm.test("Phase is OK", function (){
-			jsonBody = pm.response.json();
-			pm.expect(jsonBody.data[2].id).to.equal("EXE");
-		})
-	}
-
-	var TGetReferencialZones = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
-	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
-		
-		
-		pm.test("Zone is OK", function (){
-			jsonBody = pm.response.json();
-			pm.expect(jsonBody.data[4].id).to.equal("TZ");
-		})
-	}
-
-	var TGetReferencialDocumentType = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
-	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
-		
-		
-		pm.test("DocumentType is OK", function (){
-			jsonBody = pm.response.json();
-			pm.expect(jsonBody.data[30].id).to.equal("NOT");
-		})
-	}
-	
-	var TGetReferencialLevel = function() {
-		pm.test("Response is OK", function() {
-			pm.response.to.be.success
-			pm.expect(pm.cookies.has('AGL_LSSO'), 'AGL_LSSO').to.be.true
-			pm.response.to.have.header("Content-Type", "application/json;charset=utf-8")
-			pm.response.to.have.jsonBody()
-		})
-	
-		pm.test("Payload is OK", function() {
-			pm.response.to.have.jsonBody("success", true)
-			pm.response.to.have.jsonBody("data")
-			pm.response
-		});	
-		
-		
-		pm.test("Level is OK", function (){
-			jsonBody = pm.response.json();
-			pm.expect(jsonBody.data[7].id).to.equal("TN");
-		})
+		pm.test('RolesDimensions are OK', function () {
+			const visitRoles = profile.societe.rolesDimensions.filter(x => x.id.startsWith('absvisit.')).reduce((result, x) => {
+				result.push(x.id);
+				return result
+			}, []) 
+			pm.expect(visitRoles).to.deep.equal(rolesVisit)
+			
+			const gedRoles = profile.societe.rolesDimensions.filter(x => x.id.startsWith('absged.')).reduce((result, x) =>{
+				result.push(x.id);
+				return result
+			}, [])
+			pm.expect(gedRoles).to.deep.equal(rolesGed)
+		});
 	}
 	
 	
